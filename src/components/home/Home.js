bf3530c4e0b6ef2b0cb37458c74385b359e9c0dc
frustrabe <lukas.forner@gmail.com>
2024-06-30 13:38:21 +0200

    updated profile picture

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Style from "./Home.module.scss";
-import me from "../../img/self2.avif";
+import me from "../../img/self3.avif";
 import classNames from "classnames";
 import EmojiBullet from "./EmojiBullet";
 import SocialIcon from "./SocialIcon";
